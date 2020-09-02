7a5171065293990bd400c75816fa8f37e541d210
abrahamarslan <arslan@abrahamarslan.com>
2020-09-02 23:17:00 +0530

    #9 - Services

--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -3,6 +3,7 @@ import { Observable } from 'rxjs';
 import { Course } from 'src/model/course';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { filter, map } from 'rxjs/operators';
+import {CourseServiceService} from '../services/course-service.service'
 
 @Component({
   selector: 'app-course',
@@ -14,20 +15,11 @@ export class CourseComponent implements OnInit {
   courses: Observable<Course[]>;
   beginner: Observable<Course[]>;
   advanced: Observable<Course[]>;
-  constructor(private db: AngularFirestore) { 
+  constructor(private courseService: CourseServiceService) { 
   }
 
   ngOnInit(): void {
-      this.courses = this.db.collection('courses')
-                      .snapshotChanges()
-                      .pipe(map(snaps => {
-                        return snaps.map(snap => {
-                          return <Course> {
-                            id: snap.payload.doc.id,
-                            ...snap.payload.doc.data() as {}                    
-                          }
-                        })
-                      }));
+      this.courses = this.courseService.getCourses();
       this.beginner = this.courses.pipe(map(courses => courses.filter(course => course.categories.includes("BEGINNER"))));
       this.advanced = this.courses.pipe(map(courses => {
         return courses.filter(course => course.categories.includes("ADVANCED"));
@@ -41,5 +33,4 @@ export class CourseComponent implements OnInit {
         // })
       //});
   }
-
 }
---
+++ src/app/services/course-service.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { filter, map, first } from 'rxjs/operators';
+import { Course } from 'src/model/course';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CourseServiceService {
+
+  constructor(private db: AngularFirestore) { }
+
+  getCourses(): Observable<Course[]> {
+    return this.db.collection('courses')
+                      .snapshotChanges()
+                      .pipe(map(snaps => {
+                        return snaps.map(snap => {
+                          return <Course> {
+                            id: snap.payload.doc.id,
+                            ...snap.payload.doc.data() as {}                    
+                          }
+                        })
+                      }), first()); //First will get Observable connection only once.
+  }
+}
